fb5d6c3dc1405d1367e831e3dfbad4b10d324ea5
nitishaagarwal159 <nitishaagarwal15996@gmail.com>
2017-12-24 09:07:42 +0530

    commit

--- SignUp/settings.py
+++ SignUp/settings.py
@@ -123,6 +123,10 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS=['*']
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static"),
